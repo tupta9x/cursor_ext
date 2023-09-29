1e1140fc8776bc64031755b6898894745f5c4af9
ATUDEV <tupta1999@gmail.com>
2023-09-29 09:13:30 +0700

    cursor model example

---
+++ example/cursor.js
@@ -0,0 +1,13 @@
+export class Cursor {
+    constructor(id, name, image, cursorPath, pointerPath, collectionName, collectionId, width, height) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.cursorPath = cursorPath;
+        this.pointerPath = pointerPath;
+        this.collectionName = collectionName;
+        this.collectionId = collectionId;
+        this.width = width;
+        this.height = height;
+    }
+}
